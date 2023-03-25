56b8f17fea695c5902a69159c628d4fbfc41a4ad
Aiana Abdyrakhmanova <73655002+luianqi@users.noreply.github.com>
2023-03-25 16:51:23 +0600

    set up db

--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,30 @@
 package database
+
+import "errors"
+
+func AddProductToCart() {
+
+}
+
+// custom errors
+var (
+	ErrCannotFindProduct    = errors.New("Cannot find the product")
+	ErrCannotDecodeProducts = errors.New("Cannot find the product")
+	ErrUserIdIsNotValid     = errors.New("User is not valid")
+	ErrCannotUpdateUser     = errors.New("Cannot add this product to the cart")
+	ErrCannotRemoveItem     = errors.New("Cannot remove this item from the cart")
+	ErrCannotGetItem        = errors.New("Was unable to get item from the cart")
+	ErrCannotBuyCartItem    = errors.New("Cannot update the purchase")
+)
+
+func RemoveCartItem() {
+
+}
+
+func BuyItemFromCart() {
+
+}
+
+func InstantBuyer() {
+
+}
